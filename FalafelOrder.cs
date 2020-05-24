725c9d69aab6d6cc8234c042e655c812e3aa982c
Kfir-G <kfirg15@gmail.com>
2020-05-24 17:01:58 +0300

    pro+meth in DishOrder
    Add properties and methods of the DishOrder Class

--- FalafelOrder.cs
+++ FalafelOrder.cs
@@ -7,9 +7,9 @@ namespace HW_5
     class FalafelOrder : DishOrder
     {
         //----data fields----
-        //string name
-        //float price
-        // static numOfDishes
+        //string name from->DishOrder
+        //float price from->DishOrder
+        // static numOfDishes from->DishOrder
         protected int numOfFalafelDishes;
         protected int falafelBalls; // ONLY 0,3,6,9
         protected bool hasTahini; // if there's tahini aside
